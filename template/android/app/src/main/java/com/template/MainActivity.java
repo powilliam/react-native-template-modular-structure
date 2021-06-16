a528a0296f9f1c7bb20dd70792e4c65c2ac516b2
William Porto <william.porto@nave.rs>
2021-06-15 23:50:19 -0300

    🐛 (android): overriding onConfigurationChanged to handle ui configuration changes

--- template/android/app/src/main/java/com/template/MainActivity.java
+++ template/android/app/src/main/java/com/template/MainActivity.java
@@ -1,8 +1,16 @@
 package com.template;
 
 import com.facebook.react.ReactActivity;
+import android.content.res.Configuration;
 
 public class MainActivity extends ReactActivity {
+
+  @Override
+  public void onConfigurationChanged(Configuration configuration) {
+    super.onConfigurationChanged(configuration);
+    getReactInstanceManager()
+      .onConfigurationChanged(this, configuration);
+  }
 
   @Override
   protected String getMainComponentName() {
